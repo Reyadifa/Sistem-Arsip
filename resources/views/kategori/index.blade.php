c24f7575de581e1e04625cc0eace672efcb0a077
MuhammadFikriiii <tpublicml@gmail.com>
2024-10-24 09:35:07 +0800

    kategori

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -51,25 +51,25 @@
                     </div>
                 </div>                
 
-                <div class="overflow-hidden bg-white shadow-md rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200 border-2 ">
-                        <thead class="bg-gray-200 text-black font-bold">
+                <div class="overflow-hidden bg-white shadow-md rounded-lg border border-black">
+                    <table class="min-w-full divide-y divide-gray-200 border border-black">
+                        <thead class="bg-blue-500 text-black font-bold border border-black">
                             <tr class="">
-                                <th class="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">No</th>
-                                <th class="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider"><span class="font-bold"> Nama
+                                <th class="px-2 py-2 text-center border border-black text-xs uppercase text-white font-extrabold tracking-wider">No</th>
+                                <th class="px-6 py-3 text-center border border-black text-xs uppercase text-white font-extrabold tracking-wider"><span class="font-bold"> Nama
                                     Kategori</span></th>
-                                <th class="px-6 py-3 text-center text-xs font-medium uppercase tracking-wider">Aksi</th>
+                                <th class="px-6 py-3 text-center border border-black text-xs uppercase text-white font-extrabold tracking-wider">Aksi</th>
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
                             @foreach ($kategoris as $index =>  $kategori)
                                 <tr>
-                                    <td class="px-4 py-3 text-center text-xs font text-black-500">
+                                    <td class="px-2 py-2 border border-black text-center text-xs font text-black-500">
                                         {{ $kategoris->firstItem() + $index }} <!-- Perbaikan penggunaan $index -->
                                     </td>
-                                    <td class="px-6 py-4 text-center whitespace-nowrap text-sm text-gray-900">
+                                    <td class="px-6 py-4 border border-black text-center whitespace-nowrap text-sm text-gray-900">
                                         {{ $kategori->nama_kategori }}</td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                    <td class="px-6 py-4 border border-black whitespace-nowrap text-sm font-medium">
                                         <div class="flex space-x-4 justify-center">
                                             {{-- edit --}}
                                             <a href="{{ route('kategori.edit', $kategori->id_kategori) }}"
