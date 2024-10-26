716dbfead90058729437b17f65c6e4a7885d6e45
Hidayat3118 <muhammadhidayat3118@gmail.com>
2024-10-26 08:45:23 +0800

    tambah dashboad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     {{-- Google Font --}}
     <link href="https://fonts.googleapis.com/css2?family=Modak&display=swap" rel="stylesheet">
+   
+    
+
 
     {{-- tailwind config --}}
     @vite('resources/css/app.css')
